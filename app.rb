6cc9912e55701b10c3311228d0fcfe73e355594b
jfrag019 <jfrag019@fiu.edu>
2019-04-02 17:03:59 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,13 +3,13 @@ require 'rest-client'
 
 get '/tree-permits' do
 
-yesterday = Date.today-1
+today = Date.today
 twoDaysBefore = Date.today-2
 
-yesterday_s = yesterday.strftime("%F")
+today_s = today.strftime("%F")
 twoDaysBefore_s = twoDaysBefore.strftime("%F")
 
-url = 'https://services1.arcgis.com/CvuPhqcTQpZPT9qY/arcgis/rest/services/Tree_Permits/FeatureServer/0/query?where=1%3D1+AND+%28ReviewStatus+LIKE+%27Approved%27+OR+ReviewStatus+LIKE+%27Intended+decision%27%29+AND+PropertyAddress+IS+NOT+NULL+AND+ReviewStatusChangedDate+%3E%3D+%27'+twoDaysBefore_s+'%27+AND+ReviewStatusChangedDate+%3C%3D+%27'+yesterday_s+'%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token='
+url = 'https://services1.arcgis.com/CvuPhqcTQpZPT9qY/arcgis/rest/services/Tree_Permits/FeatureServer/0/query?where=1%3D1+AND+%28ReviewStatus+LIKE+%27Approved%27+OR+ReviewStatus+LIKE+%27Intended+decision%27%29+AND+PropertyAddress+IS+NOT+NULL+AND+ReviewStatusChangedDate+%3E+%27'+twoDaysBefore_s+'%27+AND+ReviewStatusChangedDate+%3C+%27'+today_s+'%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token='
 
 response = RestClient.get url
 
