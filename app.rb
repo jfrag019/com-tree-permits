df0e605d400359e3ba38b6e11d462d50c19f4853
jfrag019 <jfrag019@fiu.edu>
2019-04-11 10:46:33 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'rest-client'
 get '/tree-permits' do
 
 today = Date.today-8
-twoDaysBefore = Date.today-10
+twoDaysBefore = Date.today-9
 
 today_s = today.strftime("%F")
 twoDaysBefore_s = twoDaysBefore.strftime("%F")
